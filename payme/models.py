20308f5124fee145f1f5f86de248d59a1c3c2d8c
Ashok Raavi <ashok.raavi@gmail.com>
2014-07-05 16:02:58 +0530

    init models & views

--- payme/models.py
+++ payme/models.py
@@ -1,3 +1,15 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    mobile_number = models.CharField(max_length=10)
+    is_active = models.BooleanField()
+    current_balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Transaction(models.Model):
+    from_account = models.ForeignKey(Account, related_name='debits')
+    to_account = models.ForeignKey(Account, related_name='credits')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
--- payme/views.py
+++ payme/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+
+
+from payme.models import Account, Transaction
+
+'''
+1. Read SMS 
+2. Show Transactions
+'''
+
+def parse_sms(message):
+    # TODO - return from_number, to_number, decimal of amount
+    pass
+
+
+def make_payment(request):
+    from_number, to_number, amount = parse_sms(request.GET['message'])
+
+    try:
+        from_account - Account.objects.get(mobile_number=from_number)
+    except Account.DoesNotExist:
+        # TODO: return JSON response not a valid from account
+        pass
+
+    if from_account.current_balance < amount:
+        # TODO: return JSON response with Message
+        pass
+
+    to_account = Account.objects.get_or_create(mobile_number=to_number)
+
