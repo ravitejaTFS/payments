45179b33ab3ee8358a5c8eae96a178248802e085
root <root@static.46-4-64-209.clients.your-server.de>
2014-07-05 16:23:21 +0200

    merchant mobile is now char field

--- payme/models.py
+++ payme/models.py
@@ -20,7 +20,7 @@ class Transaction(models.Model):
 
 class Merchant(models.Model):
     merchant_id = models.CharField(unique=True, max_length=20)
-    merchant_mobile = models.IntegerField(unique=True, max_length=10)
+    merchant_mobile = models.CharField(unique=True, max_length=10)
     account = models.ForeignKey(Account)
     description = models.TextField()
 
